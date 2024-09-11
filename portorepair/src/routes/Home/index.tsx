652f070086e328fd84f9e420804b4575fff13500
Gabriel Marques de Lima Sousa <gabrielmarquesprofissional2005@gmail.com>
2024-09-11 18:39:28 -0300

    Criando a pasta routes e criando a rota Home, já configurada.

---
+++ portorepair/src/routes/Home/index.tsx
@@ -0,0 +1,11 @@
+import BannerHome from '../../components/Home/BannerHome';
+import ServicesSection from '../../components/Home/ServicesSection';
+
+export default function Home(){
+    return (
+        <main>
+            <BannerHome />
+            <ServicesSection />
+        </main>
+    )
+}
