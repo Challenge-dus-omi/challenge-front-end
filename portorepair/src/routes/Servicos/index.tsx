359cef71646322b8dd8c400cbd094bb8dbc38b63
Gabriel Marques de Lima Sousa <gabrielmarquesprofissional2005@gmail.com>
2024-09-11 20:29:18 -0300

    Adicionando o LoginForm na routes servicos

--- portorepair/src/routes/Servicos/index.tsx
+++ portorepair/src/routes/Servicos/index.tsx
@@ -1,9 +1,11 @@
 import BannerServicos from "../../components/Servicos/BannerServicos";
+import LoginForm from "../../components/Servicos/LoginForm";
 
 export default function Servicos(){
     return (
         <main>
             <BannerServicos />
+            <LoginForm />
         </main>
     )
 }
