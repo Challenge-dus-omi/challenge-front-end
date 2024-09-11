2af00f1e834c7a0ad1ca2c88536cc241ab48a1d1
Gabriel Marques de Lima Sousa <gabrielmarquesprofissional2005@gmail.com>
2024-09-11 20:03:46 -0300

    Alterando link do services section

--- portorepair/src/components/Home/ServicesSection.tsx
+++ portorepair/src/components/Home/ServicesSection.tsx
@@ -10,20 +10,20 @@ export default function ServicesSection() {
         imageSrc={orcamentoImage}
         title="Orçamento Online"
         description="Faça um orçamento certeiro online, realizado de forma automática em poucos minutos utilizando a nossa nova técnologia SeguroRepair, a nossa Inteligência Artificial que irá fazer o seu orçamento e entregar o preço total."
-        link="/servicos#chatbot"
+        link="/servicos"
         buttonText="Fazer Orçamento Online"
       />
       <SolucaoCard imageSrc={agendamentoImage}
         title="Agendamento de serviços"
         description="Faça o seu agendamento para as manutenções necessárias na unidade mais próxima!"
-        link="/servicos#chatbot"
+        link="/servicos"
         buttonText="Fazer Agendamento"
       />
       <SolucaoCard
         imageSrc={portoRepairImage}
         title="PortoRepair"
         description="Faça um orçamento certeiro online, realizado de forma automática em poucos minutos utilizando a nossa nova tecnologia SeguroRepair, a nossa Inteligência Artificial."
-        link="/servicos#chatbot"
+        link="/servicos"
         buttonText="Acessar PortoRepair"
       />
     </div>
